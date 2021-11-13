53ccc1069b9b60458d1bb3683abb4d0bc0d0f708
Yeji Kwon <yk1932@nyu.edu>
2021-11-13 15:23:10 -0500

    adding website

--- website/main.js
+++ website/main.js
@@ -1,7 +1,68 @@
 const parallax = document.getElementById("parallax");
-const boxHolder = document.getElementById("BoxDisappear")
+const boxHolder = document.getElementById("BoxDisappear");
 
-window.addEventListener("scroll",function(){
-    let offset = window.pageYOffset;
-    parallax.style.backgroundPositionY = -offset*0.2 + "px";
+
+//Parallax Scrolling
+
+    window.addEventListener("scroll",function(){
+        let offset = window.pageYOffset;
+        parallax.style.backgroundPositionY = -offset*0.2 + "px";
+    })
+
+
+// document.getElementById("titleText1").addEventListener("click",function(){
+//     console.log("i have been clicked!")
+// })
+
+//Scroll Up Button
+
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  window.scrollTo({top: 0, behavior: 'smooth'});
+}
+
+document.getElementById("workTwentyTwentyOne").style.display = "block";
+document.getElementById("twentytwentyone").classList.add("view");
+
+
+document.getElementById("twentytwentyone").addEventListener("click",function(){
+    window.scrollTo({top: 0, behavior: 'smooth'});
+    document.getElementById("workTwentyNineteen").style.display = "none";
+    document.getElementById("twentynineteen").classList.remove("view");
+    document.getElementById("workTwentyTwenty").style.display = "none";
+    document.getElementById("twentytwenty").classList.remove("view");
+
+    document.getElementById("workTwentyTwentyOne").style.display = "block";
+    document.getElementById("twentytwentyone").classList.add("view");
+
 })
+
+document.getElementById("twentytwenty").addEventListener("click",function(){
+    window.scrollTo({top: 0, behavior: 'smooth'});
+    document.getElementById("twentytwenty").classList.add("view");
+
+    document.getElementById("workTwentyNineteen").style.display = "none";
+    document.getElementById("twentynineteen").classList.remove("view");
+    document.getElementById("workTwentyTwentyOne").style.display = "none";
+    document.getElementById("twentytwentyone").classList.remove("view");
+
+    document.getElementById("workTwentyTwenty").style.display = "block";
+})
+
+document.getElementById("twentynineteen").addEventListener("click",function(){
+    window.scrollTo({top: 0, behavior: 'smooth'});
+    document.getElementById("workTwentyTwenty").style.display = "none";
+    document.getElementById("twentytwenty").classList.remove("view");
+    document.getElementById("workTwentyTwentyOne").style.display = "none";
+    document.getElementById("twentytwentyone").classList.remove("view");
+
+    document.getElementById("workTwentyNineteen").style.display = "block";
+    document.getElementById("twentynineteen").classList.add("view");
+
+
+})
+
+
